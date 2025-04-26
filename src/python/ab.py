dd7ad50e4b412e3d210160e00dcf1f1fd90d2f3a
George Liu <125779780+iXhong@users.noreply.github.com>
2025-04-26 14:50:43 +0800

    -Refactor: mv gausslegendreGrid() into module.py
    - refactor the ab.py
    - add module.py for reuse functions

--- src/python/ab.py
+++ src/python/ab.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.special import roots_legendre
+from module import gausslegendreGrid
 
 #define parameter
 D = (0.8**3)/0.5
@@ -24,35 +24,6 @@ def g(k2):
     part3 = (1-np.exp(-k2/(4*mt**2)))/k2
 
     return part1 + part2 * part3
-
-
-def gausslegendreGrid(xp1,xp2,Nz,Np):
-    """
-    x, w in Gauss-Legendre points
-    Paramters:
-        xp1: left endpoint of momentum p^2
-        xp2: right endpoint of momentum p^2
-
-    Return:
-        xz,wz: Guass-legendre point in [-1,1]
-        xp,wp: Guass-legendre point combined with log transform in [xp1,xp2]
-
-    Note:
-        x stands for grid points array; w stands for weight array
-    """
-    #gausslegendre points for angular integration 
-    xz, wz = roots_legendre(Nz)
-    #gausslegendre points for momentum integration
-    xp, wp = roots_legendre(Np)
-    # transform the points from [-1,1] to [-4,4]
-    xp1_log, xp2_log = np.log10(xp1), np.log10(xp2)
-    xp = (xp2_log-xp1_log)*xp/2 + (xp1_log+xp2_log)/2
-    wp = (xp2_log-xp1_log)/2 * wp
-    #log transform to [1e-4,1e4]
-    xp = 10**xp
-    wp = wp*xp*np.log(10)
-
-    return xz,wz,xp,wp
 
 
 def solver(Nz:int,Np:int,xmin:float,xmax:float,max_iter:int,eps:float):
@@ -143,7 +114,7 @@ if __name__ == "__main__":
     max_iter = 1000
     eps = 1e-3
 
-    z2,z4,reA,reB,xp = solver(Np=200,Nz=25,xmin=p2_min,xmax=p2_max,max_iter=max_iter,eps=eps)
+    z2,z4,reA,reB,xp = solver(Np=100,Nz=25,xmin=p2_min,xmax=p2_max,max_iter=max_iter,eps=eps)
 
     file = "./abdat.npz"
     M = reB / reA
---
+++ src/python/module.py
@@ -0,0 +1,36 @@
+"""
+@name module.py
+@author George Liu
+@since 2025.04
+"""
+from scipy.special import roots_legendre
+import numpy as np
+
+
+def gausslegendreGrid(xp1,xp2,Nz,Np):
+    """
+    x, w in Gauss-Legendre points
+    Paramters:
+        xp1: left endpoint of momentum p^2
+        xp2: right endpoint of momentum p^2
+
+    Return:
+        xz,wz: Guass-legendre point in [-1,1]
+        xp,wp: Guass-legendre point combined with log transform in [xp1,xp2]
+
+    Note:
+        x stands for grid points array; w stands for weight array
+    """
+    #gausslegendre points for angular integration 
+    xz, wz = roots_legendre(Nz)
+    #gausslegendre points for momentum integration
+    xp, wp = roots_legendre(Np)
+    # transform the points from [-1,1] to [-4,4]
+    xp1_log, xp2_log = np.log10(xp1), np.log10(xp2)
+    xp = (xp2_log-xp1_log)*xp/2 + (xp1_log+xp2_log)/2
+    wp = (xp2_log-xp1_log)/2 * wp
+    #log transform to [1e-4,1e4]
+    xp = 10**xp
+    wp = wp*xp*np.log(10)
+
+    return xz,wz,xp,wp
